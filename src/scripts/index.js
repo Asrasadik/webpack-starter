9be2b00a8bb0450c86ad1eff7e7bb004c91220bc
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2022-08-15 01:54:43 +0530

    Rest parameters, Destructuring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,44 @@
-import '../styles/index.scss';
+/*import '../styles/index.scss';
 
-console.log('webpack starterkit');
+console.log('webpack starterkit');*/
+
+/*1. console.log(carId);
+console.log(carId2)
+
+let carId = 46;
+//or
+var carId2 = 46;
+
+2. if (true) {
+  let foo = 9;
+  //or
+  var foo2 =9;
+}
+console.log(foo);
+console.log(foo2);
+
+3. function sendCars(day, ...allCarIds) {
+  console.log(day);
+  allCarIds.forEach((id) => console.log(id));
+}
+sendCars("Monday", 100, 200, 300);*/
+
+let carIds = [1, 2, 5];
+
+let [car1, car2, car3] = carIds;
+
+console.log(car1, car2, car3);
+
+//------------Rest Parameters----------------
+
+let cars, remainingIds;
+[, ...remainingIds] = carIds;
+
+console.log(cars, remainingIds);
+
+//--------destructuring objects------
+
+let car = { id: 500, style: "convertible" };
+let id, style;
+({ id, style } = car);
+console.log(id, style);
